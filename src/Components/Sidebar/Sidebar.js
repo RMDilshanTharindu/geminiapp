4911d96c6ff706dba900a267f8794cbdddf32b26
Dilshan_Tharindu <dilshantharinducamp@gmail.com>
2025-04-13 14:41:23 +0530

    Add Functionality

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,36 +1,43 @@
 import './Sidebar.css'
 import { assets } from '../../assets/assets'
+import { useState } from 'react'
 
 const Sidebar = () => {
+
+    const[extended,setExtended] = useState(false);
+
   return (
     <div className='sidebar'>
         <div className='top'>
-            <img className='menu' src={assets.menu_icon} alt='' /><br/>
+            <img onClick={()=>setExtended(pre=>!pre)} className='menu' src={assets.menu_icon} alt='' /><br/>
             <div className='new-chat'>
                 <img src={assets.plus_icon} alt='' />
-                <p>New chat</p>
+                {extended?<p>New chat</p>:null}
             </div>
-            <div className='recent'>
-                <p className='recent-title'>Recent</p>
-                <div className='recent-entry'>
-                     <img src={assets.message_icon} alt='' />
-                     <p>What is React...</p>
-                </div>
-            </div>
+            {extended?
+              <div className='recent'>
+              <p className='recent-title'>Recent</p>
+              <div className='recent-entry'>
+                   <img src={assets.message_icon} alt='' />
+                   <p>What is React...</p>
+              </div>
+          </div>
+            :null }
+            
         </div>
 
         <div className='bottom'>
             <div className='bottom-item recent-entry'>
                 <img src={assets.question_icon} alt='' />
-                <p>Help</p>
+                {extended?<p>Help</p>:null}
             </div>
             <div className='bottom-item recent-entry'>
                 <img src={assets.history_icon} alt='' />
-                <p>History</p>
+                {extended?<p>History</p>:null}
             </div>
             <div className='bottom-item recent-entry'>
                 <img src={assets.setting_icon} alt='' />
-                <p>Setting</p>
+                {extended?<p>Setting</p>:null}
             </div>
 
         </div>
