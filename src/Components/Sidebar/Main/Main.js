ca6e9fc458d493ac0d1edf5164b7f4329ed391ab
Dilshan_Tharindu <dilshantharinducamp@gmail.com>
2025-04-14 22:46:14 +0530

    Remove some unwanted code parts:1

--- src/Components/Sidebar/Main/Main.js
+++ src/Components/Sidebar/Main/Main.js
@@ -63,22 +63,7 @@ const Main = () => {
                         </div>
                     ))}
                 </div>
-                {/* <div className='result-title'>
-                    <img src={assets.user_icon} alt='' />
-                    <p>{recentPrompt}</p>
-                </div>
-                <div className='result-data'>
-                    <img src={assets.gemini_icon} alt='' /> 
-                    {loading
-                    ?<div className='loader'>
-                        <hr/>
-                        <hr/>
-                        <hr/>
-                    </div>
-                    :<p dangerouslySetInnerHTML={{__html:response}}></p> 
-                     }
-                    
-                </div> */}
+                
                 
             </div>
             }
