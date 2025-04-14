aa48d391326d791740284de2ee035d8ad1836916
Dilshan_Tharindu <dilshantharinducamp@gmail.com>
2025-04-14 16:06:21 +0530

    add some css and Create the pre loader

--- src/Components/Sidebar/Main/Main.js
+++ src/Components/Sidebar/Main/Main.js
@@ -50,7 +50,15 @@ const Main = () => {
                 </div>
                 <div className='result-data'>
                     <img src={assets.gemini_icon} alt='' />
-                    <p dangerouslySetInnerHTML={{__html:response}}></p> 
+                    {loading
+                    ?<div className='loader'>
+                        <hr/>
+                        <hr/>
+                        <hr/>
+                    </div>
+                    :<p dangerouslySetInnerHTML={{__html:response}}></p> 
+                     }
+                    
                 </div>
             </div>
             }
